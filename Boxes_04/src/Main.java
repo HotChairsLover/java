2064ca122f90f0fea383e9ec3eb412355e30ba77
HotChairsLover <51674989+GeraltGwint@users.noreply.github.com>
2021-05-16 01:15:05 +0500

    Add files via upload

---
+++ Boxes_04/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        trucksCount(125451);
+
+    }
+
+    public static void trucksCount(double boxCount) {
+        int trucks = 0;
+        int containers = 0;
+        int boxes;
+        int containersCount = 0;
+
+        while (boxCount > 0) {
+            trucks++;
+            System.out.println("Грузовик " + trucks + ":");
+            for (containers = 1; containers <= 12 & boxCount > 0; containers++) {
+                if (boxCount <= 0) {
+                    return;
+                }
+                containersCount++;
+                System.out.println("Контейнер " + containers + ":");
+                for (boxes = 1; boxes <= 27 & boxCount > 0; boxes++) {
+                    System.out.println("\tЯщик " + boxes);
+                    boxCount--;
+                }
+            }
+        }
+        System.out.println("Необходимо: ");
+        System.out.println("Грузовиков - " + trucks);
+        System.out.println("Контейнеров - " + containersCount);
+    }
+}
+
